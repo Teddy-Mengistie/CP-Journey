d95d2bfa3481341c52e8d206d0fb8701ac78c8af
Teddy M <55628361+Teddy-Mengistie@users.noreply.github.com>
2021-06-26 10:44:45 -0400

    Update Leetcode1Improved.java

--- Leetcode1Improved.java
+++ Leetcode1Improved.java
@@ -1,1 +1,17 @@
-
+class Solution {
+  public List<List<Integer>> subsets(int[] nums) {
+     List<List<Integer>> ret = new ArrayList<>();
+        help(0, nums, ret, new ArrayList<>());
+        return ret;
+    }
+    public void help(int index, int[] nums, List<List<Integer>> list, List<Integer> curr){
+        if(index == nums.length){
+            list.add(new ArrayList(curr));
+            return;
+        }
+        curr.add(nums[index]);
+        help(index + 1, nums, list, curr);
+        curr.remove(curr.size()-1);
+        help(index + 1, nums, list, curr);
+    }
+} 
