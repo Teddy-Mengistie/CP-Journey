88c7a3e0e3cdad1b4d3d9cff062ad2ac201af1c6
Teddy :] <Tewodros.mengistie@gmail.com>
2023-09-24 22:10:59 -0400

    codeforces added

--- Leetcode13.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int percentageLetter(String s, char letter) {
-        int charCount = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == letter) charCount ++;
-        }
-        return (int) (((double)charCount/s.length()) * 100);
-    }
-}
---
+++ cf/practice/1822E.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    for (; t > 0; t--) {
+        int n;
+        cin>>n;
+        char s[n];
+        fgets(s, n+1, stdin);
+        int swaps = 0;
+        cout << swaps << "\n";
+    }
+    
+    return 0;
+}
